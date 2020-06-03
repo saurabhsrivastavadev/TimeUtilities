b621f17d214d5019816db0f8a67d8ddce20d6c9d
Saurabh Srivastava <sobu86@gmail.com>
2020-06-04 01:40:02 +0530

    Adding timer to continuously update UTC time on screen

---
+++ TimeUtilities/Pages/Index.razor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TimeUtilities.Pages
+{
+    public partial class Index
+    {
+        // Constants 
+        private readonly DateTime unixEpoch = DateTime.UnixEpoch;
+        private readonly DateTime gpsEpoch = DateTime.Parse("6 January 1980");
+        private readonly long gpsUtcEpochDeltaMillis;
+        private readonly TimeZoneInfo tzInfo = TimeZoneInfo.Local;
+        private readonly Timer timer = new Timer(100);
+
+        // Variables
+        private DateTime utcNow;
+        private DateTime gpsNow;
+        private long utcNowMillis;
+        private long gpsNowMillis;
+
+        public Index()
+        {
+            gpsUtcEpochDeltaMillis = (long)gpsEpoch.Subtract(unixEpoch).TotalMilliseconds;
+
+            timer.Elapsed += async (sender, e) => await TimerTick();
+            timer.Start();
+
+            Console.WriteLine("Index page constructor !");
+        }
+
+        private Task TimerTick()
+        {
+            // populate instance variables
+            utcNow = DateTime.UtcNow;
+            gpsNow = utcNow.Subtract(TimeSpan.FromMilliseconds(gpsUtcEpochDeltaMillis));
+            utcNowMillis = (long)utcNow.Subtract(unixEpoch).TotalMilliseconds;
+            gpsNowMillis = (long)gpsNow.Subtract(gpsEpoch).TotalMilliseconds;
+
+            // update the UI
+            this.StateHasChanged();
+            return Task.CompletedTask;
+        }
+    }
+}
--- TimeUtilities/Program.cs
+++ TimeUtilities/Program.cs
@@ -14,10 +14,14 @@ namespace TimeUtilities
     {
         public static async Task Main(string[] args)
         {
+            Console.WriteLine("TimeUtilities::Main()");
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new HttpClient {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
 
             await builder.Build().RunAsync();
         }
