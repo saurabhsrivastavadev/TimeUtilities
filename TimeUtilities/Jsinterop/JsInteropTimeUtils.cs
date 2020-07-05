570d054adc6803997d8fb89695d8554b7d647031
Saurabh Srivastava <sobu86@gmail.com>
2020-07-05 13:14:13 +0530

    Adding JS Interop code for fetching timezone info

---
+++ TimeUtilities/Jsinterop/JsInteropTimeUtils.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeUtilities.Jsinterop
+{
+    internal class JsInteropTimeUtils
+    {
+        // Singleton Instance
+        public static JsInteropTimeUtils Instance { get; private set; }
+        private JsInteropTimeUtils() { }
+
+        // Javascript runtime
+        // Must be set before using this class
+        private static IJSRuntime _jsr;
+        public static IJSRuntime JSR
+        {
+            private get
+            {
+                return _jsr;
+            }
+
+            set
+            {
+                _jsr = value;
+                if (Instance == null)
+                {
+                    Instance = new JsInteropTimeUtils();
+                }
+            }
+        }
+
+        // Returns the offset from UTC in minutes
+        public async Task<int> GetLocalTimezoneOffset()
+        {
+            return await JSR.InvokeAsync<int>("getLocalTimezoneOffset");
+        }
+
+        public async Task<string> GetLocalTimezoneName()
+        {
+            return await JSR.InvokeAsync<string>("getLocalTimezoneName");
+        }
+    }
+}
---
+++ TimeUtilities/wwwroot/jsinterop.js
@@ -0,0 +1,10 @@
+﻿/* File containing functions for JS Interop */
+
+// Get timezone offset for current locale
+window.getLocalTimezoneOffset = () => {
+    return new Date().getTimezoneOffset();
+}
+
+window.getLocalTimezoneName = () => {
+    return Intl.DateTimeFormat().resolvedOptions().timeZone;
+}
